6b740e3757f114c3dfedc903d267828f78a9cc5b
hakhan1999 <khanhassaan961@gmail.com>
2025-06-17 12:06:30 +0500

    simple counter

---
+++ 02Counter/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  let [counter, setCounter] = useState(0);
+
+  const increment = () => {
+    setCounter(counter + 1);
+    if (counter + 1 > 20) {
+      setCounter(20);
+    }
+  };
+  const decrement = () => {
+    setCounter(counter - 1);
+    if (counter - 1 < 0) {
+      setCounter(0);
+    }
+  };
+  const reset = () => {
+    setCounter(0);
+  };
+
+  return (
+    <>
+      <h1>Simple Counter</h1>
+      <h2>Counter Value: {counter}</h2>
+      <div className="buttons">
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+        <button onClick={reset}>Reset</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ customReact/customReact.js
@@ -0,0 +1,1 @@
+const mainCOntainer = document.getElementById('root')
