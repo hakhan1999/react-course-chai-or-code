1c05ff7e75aa9acd4b8f848d9e94a6a811266927
hakhan1999 <khanhassaan961@gmail.com>
2025-06-24 13:27:29 +0500

    Context API Discussion

---
+++ contextAPI01/src/components/Login.jsx
@@ -0,0 +1,36 @@
+import { useContext, useState } from "react";
+import UserContext from "../context/userContext";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { setUser } = useContext(UserContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUser({ username, password });
+  };
+  return (
+    <>
+      <h2>Login</h2>
+      <div className="form">
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Username"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <button onClick={handleSubmit}>Submit</button>
+      </div>
+    </>
+  );
+};
+
+export default Login;
