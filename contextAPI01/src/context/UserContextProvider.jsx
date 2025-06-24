60b95fee64b0e042ffd745ca76ae3a1bbd64b178
hakhan1999 <khanhassaan961@gmail.com>
2025-06-24 15:17:29 +0500

    Dark Theme Toggle Using Context API

--- contextAPI01/src/context/UserContextProvider.jsx
+++ contextAPI01/src/context/UserContextProvider.jsx
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import UserContext from "./userContext";
 
 const UserContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   return (
-    <UserContext.Provider value={(user, setUser)}>
+    <UserContext.Provider value={{ user, setUser }}>
       {children}
     </UserContext.Provider>
   );
 };
- export default UserContextProvider
+export default UserContextProvider;
