d701adf0eeef061000bd12e4bbeda7e53fd07260
hakhan1999 <khanhassaan961@gmail.com>
2025-07-16 13:14:38 +0500

    configure redux

--- megaBlog/src/App.jsx
+++ megaBlog/src/App.jsx
@@ -1,11 +1,43 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
 
 const App = () => {
-  return (
-    <div>
-      Mega Blog
-    </div>
-  )
-}
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-export default App
- 
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+        } else {
+          dispatch(logout());
+        }
+        setLoading(false);
+      })
+      .finally(() => {
+        dispatch(logout());
+        setLoading(false);
+      });
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-col">
+      <div className="w-full block">
+        <Header />
+        <main>
+          {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : (
+    <div>Loading...</div>
+  );
+};
+
+export default App;
